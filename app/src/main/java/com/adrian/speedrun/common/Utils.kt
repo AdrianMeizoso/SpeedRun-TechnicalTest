faf42b428c5e8fbfdc03c682edc36c66d0df337b
Adrián Meizoso <adrian.meizosolopez@altran.com>
2018-11-16 00:20:04 +0100

    Change image loading + Added person image

--- app/src/main/java/com/adrian/speedrun/common/Utils.kt
+++ app/src/main/java/com/adrian/speedrun/common/Utils.kt
@@ -1,19 +1,14 @@
 package com.adrian.speedrun.common
 
-import android.content.res.Resources
-import com.adrian.speedrun.R
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.adrian.speedrun.R
 import com.adrian.speedrun.common.injection.GlideApp
 
 @BindingAdapter("imageUrl")
 fun ImageView.setImageUrl(url: String?) {
-    GlideApp.with(context).load(url).placeholder(R.drawable.loading_anim).into(this)
+    GlideApp.with(context).load(url).placeholder(R.drawable.ic_sand_watch).into(this)
 }
-
-val Int.dp: Int get() = (this / Resources.getSystem().displayMetrics.density).toInt()
-
-val Int.px: Int get() = (this * Resources.getSystem().displayMetrics.density).toInt()
 
 val String.formattedTime: String? get() {
     val timeSplit = this
